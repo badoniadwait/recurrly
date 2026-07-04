49a8cd8aca67087d1e0ce9a1f53c8a71e18705bf
Adwait Badoni <badoniadwait@gmail.com>
2026-07-04 12:07:07 +0530

    message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
-import { Stack } from "expo-router";
 
 import "@/app/global.css";
+import { Stack } from "expo-router";
 import React from "react";
 
 export default function RootLayout() {
     return (
-    // <Stack>
-    //   <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    // </Stack>
-        <Stack screenOptions={{headerShown: false}}/>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+
+      // <Tabs screenOptions={{headerShown: false}}>
+      //       <Tabs.Screen name="(tabs)"></Tabs.Screen>
+      // </Tabs>
+        // <Stack screenOptions={{headerShown: false}}/>
     )
 }
 
